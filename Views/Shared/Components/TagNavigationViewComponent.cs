3effca42b4baf5d867f1b2fe5ddbe79b70ff5db8
GarryWest <garrywest@msn.com>
2024-09-16 16:16:48 -0500

    AB#33, AB#34 Clean up URLs, add blog post tag navigation

---
+++ Views/Shared/Components/TagNavigationViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcBlog.Data;
+
+namespace MvcBlog.Views.Shared.Components
+{
+    public class TagNavigationViewComponent : ViewComponent
+    {
+        private readonly MvcBlogContext _context;
+
+        public TagNavigationViewComponent(MvcBlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string? selectedTag)
+        {
+            ViewBag.SelectedTag = selectedTag;
+            List<string?> items = await GetItemsAsync();
+            return View(items);
+        }
+
+        private Task<List<String?>> GetItemsAsync()
+        {
+            return _context!.BlogPostTag!
+                .Select(t => t.TagName)
+                .Distinct()
+                .OrderBy(t => t)
+                .ToListAsync();
+        }
+    }
+}
